afddf8e8b66f23de4d131074e6e00d7f9d1d0c8d
yousseff <youssef@sudoai.codes>
2020-03-17 01:27:44 +0200

    week2

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -27,7 +27,7 @@ public class HelloWorld extends PApplet
 	public static String mbTilesString = "blankLight-1-3.mbtiles";
 	
 	// IF YOU ARE WORKING OFFLINE: Change the value of this variable to true
-	private static final boolean offline = false;
+	private static final boolean offline = true;
 	
 	/** The map we use to display our home town: La Jolla, CA */
 	UnfoldingMap map1;
@@ -75,6 +75,9 @@ public class HelloWorld extends PApplet
 		MapUtils.createDefaultEventDispatcher(this, map1);
 		
 		// TODO: Add code here that creates map2 
+		map2 = new UnfoldingMap(this, 450, 50, 350, 500, provider);
+		map2.zoomAndPanTo(zoomLevel, new Location(23.7959f, 90.35759f));
+		MapUtils.createDefaultEventDispatcher(this, map2);
 		// Then you'll modify draw() below
 
 	}
@@ -84,6 +87,7 @@ public class HelloWorld extends PApplet
 		// So far we only draw map1...
 		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		map2.draw();
 	}
 
 	
